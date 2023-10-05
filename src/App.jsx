69c72f62214ab02c0a1515c748a0cc3ec55d72b7
ahmedabro <ahmedabro97@gmail.com>
2023-10-06 03:24:34 +0500

    AddPost Component Added and working fine

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import PostsList from './features/posts/PostsList'
+import AddPost from './features/posts/AddPost'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <main className='App'>
-      
+      <AddPost />
+      <PostsList />
     </main>
   )
 }
